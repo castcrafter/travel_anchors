3305b872c33e995a478d94a8751f4ff6cba87600
CastCrafter <66563550+castcrafter@users.noreply.github.com>
2020-09-26 20:04:42 +0200

    Update TravelStaff.java
    teleport ding

--- src/main/java/de/castcrafter/travel_anchors/items/TravelStaff.java
+++ src/main/java/de/castcrafter/travel_anchors/items/TravelStaff.java
@@ -1,12 +1,14 @@
 package de.castcrafter.travel_anchors.items;
 
 import de.castcrafter.travel_anchors.setup.Registration;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.RegistryObject;
 
@@ -23,8 +25,18 @@ public class TravelStaff extends Item{
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
         ItemStack equipped = playerIn.getHeldItem(handIn);
-        System.out.println("rechtsklick1");
         if (playerIn.isSneaking()){
+            if (!worldIn.isRemote){
+                RayTraceResult lookingAt = Minecraft.getInstance().objectMouseOver;
+                if(lookingAt != null && lookingAt.getType() == RayTraceResult.Type.BLOCK){
+                    double x = lookingAt.getHitVec().getX();
+                    double y = lookingAt.getHitVec().getY();
+                    double z = lookingAt.getHitVec().getZ();
+
+                    playerIn.setPositionAndUpdate(x, y, z);
+                    System.out.println(lookingAt);
+                }
+            }
             System.out.println("rechtsklick2");
         }
         return super.onItemRightClick(worldIn, playerIn, handIn);
