e218c8d713c0a59a16bf0dada442b8ab38e0b0de
CastCrafter <66563550+castcrafter@users.noreply.github.com>
2020-09-29 21:18:43 +0200

    travel staff on anchor works now (limited range)

--- src/main/java/de/castcrafter/travel_anchors/items/TravelStaff.java
+++ src/main/java/de/castcrafter/travel_anchors/items/TravelStaff.java
@@ -1,17 +1,20 @@
 package de.castcrafter.travel_anchors.items;
 
-import com.sun.javafx.geom.Vec3d;
+import de.castcrafter.travel_anchors.blocks.TravelAnchorBlock;
+import de.castcrafter.travel_anchors.blocks.TravelAnchorTile;
 import de.castcrafter.travel_anchors.setup.ModSetup;
+import de.castcrafter.travel_anchors.setup.Registration;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.RayTraceContext;
-import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.*;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -31,22 +34,22 @@ public class TravelStaff extends Item {
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-        RayTraceResult trace = rayTrace(worldIn, playerIn, RayTraceContext.FluidMode.NONE);
-        RayTraceResult newtrace = Minecraft.getInstance().objectMouseOver;
+        BlockRayTraceResult trace = rayTrace(worldIn, playerIn, RayTraceContext.FluidMode.NONE);
 
         if (!worldIn.isRemote) {
+            if(trace != null && worldIn.getBlockState(trace.getPos()).getBlock() == Registration.TRAVEL_ANCHOR_BLOCK.get()){
+                if(playerIn.isSneaking()){
+                    playerIn.setPositionAndUpdate(trace.getHitVec().x, trace.getHitVec().y + 1, trace.getHitVec().z);
+                    return ActionResult.resultSuccess(playerIn.getHeldItem(handIn));
+                }
+            }
+            System.out.println(worldIn.getBlockState(trace.getPos()).getBlock());
             if (playerIn.isSneaking()) {
                 Vector3d pos = playerIn.getPositionVec();
                 float yaw = playerIn.rotationYaw * ((float) Math.PI / 180F);
                 float pitch = playerIn.rotationPitch * ((float) Math.PI / 180F);
                 playerIn.setPositionAndUpdate(pos.x - MathHelper.sin(yaw) * 5, pos.y + -MathHelper.sin(pitch) * 5, pos.z + MathHelper.cos(yaw) * 5);
                 return ActionResult.resultSuccess(playerIn.getHeldItem(handIn));
-            }
-            if(trace != null && trace.getType() == RayTraceResult.Type.BLOCK /*&& Block == TravelAnchorBlock*/){
-                if(playerIn.isSneaking()){
-                    Vec3d positionVector = new Vec3d(trace.getHitVec().x, trace.getHitVec().y, trace.getHitVec().z);
-                    playerIn.setPositionAndUpdate(positionVector.x, positionVector.y, positionVector.z);
-                }
             }
         }
 
@@ -56,4 +59,7 @@ public class TravelStaff extends Item {
     public void addInformation(ItemStack stack, @Nullable World world, List<ITextComponent> list, ITooltipFlag flags) {
         list.add(new TranslationTextComponent("message.travel_staff"));
     }
+
+
 }
+
