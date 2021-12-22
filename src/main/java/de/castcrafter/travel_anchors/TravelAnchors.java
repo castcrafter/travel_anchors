bf186c5e03ab4a5f9bd36b513aecf67546a3842a
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-12-22 09:33:18 +0100

    Fix crash on server. Closes #34

--- src/main/java/de/castcrafter/travel_anchors/TravelAnchors.java
+++ src/main/java/de/castcrafter/travel_anchors/TravelAnchors.java
@@ -6,7 +6,9 @@ import io.github.noeppi_noeppi.libx.mod.registration.ModXRegistration;
 import io.github.noeppi_noeppi.libx.mod.registration.RegistrationBuilder;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -32,7 +34,7 @@ public final class TravelAnchors extends ModXRegistration {
         network = new Networking(this);
         
         MinecraftForge.EVENT_BUS.register(new EventListener());
-        MinecraftForge.EVENT_BUS.addListener(TravelAnchorRenderer::renderAnchors);
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> MinecraftForge.EVENT_BUS.addListener(TravelAnchorRenderer::renderAnchors));
     }
 
     @Nonnull
